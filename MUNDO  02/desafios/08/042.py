d353c44073c204d0f156a71f63b1d7f32afb91bb
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2024-02-20 09:52:13 -0300

    Correção do desafio 035 concluido

--- MUNDO  02/desafios/08/042.py
+++ MUNDO  02/desafios/08/042.py
@@ -8,15 +8,8 @@ first_line = float(input('Digite o valor da primeira reta : '))
 second_line = float(input('Digite o valor da segunda reta : '))
 third_line = float(input('Digite o valor da terceira reta : '))
 
-triangle = first_line + second_line
+if first_line < second_line + third_line and second_line < first_line + third_line and third_line < first_line + second_line :
+    print('As restas acima PODEM FORMAR UM TRIANGULO')
 
-if triangle <= third_line :
-    print('OPA !!! As retas informadas conseguem formar um reta ')
-    if first_line == second_line and first_line == third_line:
-        print(f'O tringulo que sera formado sera um triangulo EQUILÁTERO, onde todos os lados são iguais')
-    elif first_line != second_line and second_line == third_line :
-        print(f'O tringulo que sera formado sera um trigunlo ISÓSCELES , onde dois lados são iguais e um lado e diferente')
-    elif first_line != second_line and first_line != third_line :
-        print(f'O trngulo que sera formado e um triangulo ESCALENO, onde são todos os lados diferentes')
 else :
-    print('Que pena as retas que você informou não e possivel formar um triangulo  ')
+    print('As retas acima NÃO PODEM FORMAR UM TRIANGULO')
--- MUNDO 01/Desafios/07/035.py
+++ MUNDO 01/Desafios/07/035.py
@@ -8,9 +8,8 @@ first_line = float(input('Digite o valor da primeira reta : '))
 second_line = float(input('Digite o valor da segunda reta : '))
 third_line = float(input('Digite o valor da terceira reta : '))
 
-triangle = first_line + second_line
+if first_line < second_line + third_line and second_line < first_line + third_line and third_line < first_line + second_line :
+    print('As restas acima PODEM FORMAR UM TRIANGULO')
 
-if triangle <= third_line :
-    print('OPA !!! As retas informadas conseguem formar um reta ')
 else :
-    print('Que pena as retas que você informou não e possivel formar um triangulo ')
+    print('As retas acima NÃO PODEM FORMAR UM TRIANGULO')
