8e019bcd0bd3a6f8c2070123cd13255586ae872b
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2024-02-24 18:44:45 -0300

    Desafio 048 realizado

---
+++ MUNDO  02/desafios/09/048.py
@@ -0,0 +1,10 @@
+""" Desafio 048
+Faça um programa que calucle a soma entre todos os numeros impares que sao multiplos de tres e que se encontram no intervalo de 1 ate 500 """
+
+soma = 0
+
+for numero in range(1, 501, 2):  # Começa de 1, vai até 500, incrementando de 2 em 2 (ímpares)
+    if numero % 3 == 0:  # Verifica se o número é múltiplo de três
+        soma += numero   # Adiciona o número à soma
+
+print(f"A soma dos números ímpares múltiplos de três no intervalo de 1 a 500 é: {soma}")
