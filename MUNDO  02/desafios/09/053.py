76e51e641b89ee9cf7b148d37cadf8cca1a73c71
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2024-02-24 18:57:01 -0300

    Desafio 053 realizado

--- MUNDO  02/desafios/09/053.py
+++ MUNDO  02/desafios/09/053.py
@@ -1,3 +1,14 @@
 """ 053
 Crie um programa que leia uma frase qualquer e diga se ela e uma palindroma desiderando os espaços .
 Ex: Apos a sopa, são palavras que sao lidas de tras pra frente e sao a mesma coisa  """
+
+frase = input("Digite uma frase: ").lower()  # Convertendo para minúsculas para garantir a comparação correta
+
+# Removendo os espaços da frase
+frase_sem_espacos = ''.join(frase.split())
+
+# Verificando se a frase sem espaços é um palíndromo
+if frase_sem_espacos == frase_sem_espacos[::-1]:
+    print("A frase é um palíndromo.")
+else:
+    print("A frase não é um palíndromo.")
