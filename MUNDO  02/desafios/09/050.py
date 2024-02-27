0c0f5bce392d53fe164153ce25b50520c8a7dbe7
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2024-02-27 13:57:28 -0300

    Correção do desafio 050 concluido

--- MUNDO  02/desafios/09/050.py
+++ MUNDO  02/desafios/09/050.py
@@ -3,11 +3,12 @@ Desenvolva um programa que leia seis numeros inteiros e mostre a soma apenas daq
 
 
 soma = 0
-
+cont = 0
 for i in range(6):
     numero = int(input(f"Digite o {i+1}º número inteiro: "))
     if numero % 2 == 0:
         soma += numero
+        cont = cont +1
 
-print(f"A soma dos números pares digitados é: {soma}")
+print(f"Você informou {cont} pares ,a soma dos números digitados é: {soma}")
 
