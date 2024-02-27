e2babe6804412866aca2c77b1a58c923b00ee2de
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2024-02-27 13:46:07 -0300

    Correção do desafio 047 concluido

--- MUNDO  02/desafios/09/047.py
+++ MUNDO  02/desafios/09/047.py
@@ -2,7 +2,7 @@
 Crie um programa que mostre na tela tods os números pares que estão no intervalo entre 1 a 50
  """
 
-for i in range(1,50):
+for i in range(2,50,2):
     n = i % 2
     if n == 0 :
         print(f'Os numeros pares de 1 a 50 sao {i}')
