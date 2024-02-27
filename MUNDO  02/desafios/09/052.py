f2755e3ee764f55d1f584fcf99479aa4078377c0
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2024-02-27 14:24:36 -0300

    Correção do desafio 052 realizado

--- MUNDO  02/desafios/09/052.py
+++ MUNDO  02/desafios/09/052.py
@@ -1,15 +1,16 @@
 """ 052
 Faça um programa que leia um numero inteiro e diga se ele e ou nao um numero primo """
 
-numero = int(input("Digite um número inteiro: "))
+num = int(input('Digite um numero: '))
+tot = 0
+for i in range (1,num +1):
+    if num % i == 0:
+        tot += 1
 
-# Um número primo é maior que 1 e é divisível apenas por 1 e por ele mesmo
-if numero > 1:
-    for i in range(2, int(numero**0.5) + 1):
-        if numero % i == 0:
-            print(f"{numero} não é um número primo.")
-            break
-    else:
-        print(f"{numero} é um número primo.")
-else:
-    print(f"{numero} não é um número primo.")
+print(f'O numero {num} foi divisivel {tot} vezes')
+
+if tot ==2 :
+    print('E por isso ele é primo')
+
+else :
+    print('E por isso ele não e primo')
