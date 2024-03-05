19a1b433ed11540abc05a6843d09ca1dc7ef5215
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2024-03-05 09:48:35 -0300

    Correção do desafio 057

--- MUNDO  02/desafios/10/057.py
+++ MUNDO  02/desafios/10/057.py
@@ -2,21 +2,9 @@
 Faça um programa que leia o sexo de uma pessoa, mas so aceite os valores 'M' ou 'F'.Caso esteja errado , peça a digitação novamente ate ter um valor correto  """
 
 
+sexo = str(input('Informe seu sexo [ M ] / [ F ]: ')).lower().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados invalidos. Por favor informe seu sexo : ')).lower().upper()[0]
 
-cont = 1
-nome = str(input('Digite seu nome : '))
 
-while cont != 0:
-    sexo = str(input('Informe seu sexo [ M ] / [ F ]: ')).lower()
-
-    if sexo == 'm':
-        novo_sexo = 'Masculino'
-    if sexo =='f':
-        novo_sexo = 'Feminino'
-
-        if sexo == 'm' or sexo == 'f':
-            cont = 0
-        else :
-            print('Parece que ouve um erro na hora de informar seu sexo ')
-
-print(f'Ola {nome} , sexo informado foi {novo_sexo}')
+print(f'Ola , sexo informado foi {sexo}')
