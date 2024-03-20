ff683b1dc21a5f6fb8b27f7f36f7a477cd24be18
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2024-03-20 09:44:46 -0300

    Correção do desafio 077

--- MUNDO 03/desafios/12/077.py
+++ MUNDO 03/desafios/12/077.py
@@ -2,5 +2,8 @@
 Crie um programa que tenha uma tupla com varias palavras (não usar acentos).Depois disso você deve mostrar para cada palavra quais sao as suas vogais """
 palavras = ('Pescar', 'Andar de bike', 'Caminhar', 'Jogo', 'Estudar', 'Saude')
 
-
-vogais = []
+for p in palavras :
+    print(f'\nNa palavra {p} possue as vogais: ', end=' ') 
+    for letra in p :
+        if letra.lower() in 'aeiou':
+            print(letra , end=' ')
